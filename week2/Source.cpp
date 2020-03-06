15cf0370cf368933290c6f7bc740720d9f38f8e3
Casey <thehumancasey@gmail.com>
2020-03-05 19:45:41 -0800

    prior to not equal operator

--- week2/Source.cpp
+++ week2/Source.cpp
@@ -1,18 +1,16 @@
-//Using an if statement 
-
-
+// Using else if to assign a letter grade
 // Casey James Ray 
 // Date: 03/05/2020
 // HW2. 
 
 
-#include <iostream>;
-    using namespace std;
+#include <iostream> 
+using namespace std;
 
     int main() {
 
         //take these as input
-        int score1 = 60, score2 = 70, score3 = 25;
+        int score1, score2, score3;
 
         cout << "Enter your first score: " << endl;
         cin >> score1;
@@ -22,14 +20,29 @@
         cin >> score3;
         
 
+               
+
         int average = (score1 + score2 + score3) / 3;
 
-            if (average > 60) {
-                cout << "Congratulations!" << endl;
+            if (average >= 60 && average < 70 ) {
+                cout << average << " - D " << endl;
+            }
+            else if (average >= 70 && average < 80){
+                cout << average << " - C" << endl;
+            }
+            else if (average >= 80 && average < 90 ) {
+                cout << average << " - B" << endl;
+            }
+            else if (average > 90) {
+                cout << average << " - A" << endl;
             }
             else {
-                cout << "Fail" << endl;
-            };
+                cout << average << "You Failed!";
+            }  ;
+
+
+
+
 
 
 
@@ -37,7 +50,5 @@
 
         return 0;
 
-
-
-
+               
     }
